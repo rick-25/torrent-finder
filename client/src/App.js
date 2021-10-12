06fcbdb7c06aef15a23f1c219a486daaecae2cef
Aditya Kumar <araj7060@gmail.com>
2021-10-12 20:17:45 +0530

    Added '/' route in server

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ class App extends React.Component {
             searchKey: null,
             torrentData: null
         };
-        this.endpoint = "http://localhost:8000";
+        this.endpoint = "http://torrent-finderz.herokuapp.com/";
     }
 
     render() {
--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 
 //For front-end static files
-app.use(express.static(path.join(__dirname,"/build")));
+/* app.use(express.static(path.join(__dirname,"/build"))); */
+app.use(express.static("build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+});
 
 //For torrent search api
 app.get("/search", routes.search);
