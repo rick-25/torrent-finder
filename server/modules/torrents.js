94f973abbce4d759032b99e124864452b814e711
Aditya Kumar <araj7060@gmail.com>
2024-02-25 11:05:58 +0530

    feat: update torrent search result limit

--- server/modules/torrents.js
+++ server/modules/torrents.js
@@ -3,9 +3,9 @@ const TorrentSearchApi = require('torrent-search-api');
 TorrentSearchApi.enableProvider('ThePirateBay');
 
 
-async function search(search_key, limit = 15) {
-    limit = Math.min(20, limit);
-    return await TorrentSearchApi.search(search_key, 'All', limit);
+async function search(search_key, limit = 100) {
+  limit = Math.min(100, limit);
+  return await TorrentSearchApi.search(search_key, 'All', limit);
 }
 
 exports.search = search;
